8a412b29c27d30ca5c5022954b14eed35ad1d700
Yeon22 <bsuyeon16@gmail.com>
2021-05-27 12:59:12 +0900

    init. static slow url checker

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request Failed")
+
+func main() {
+	var results = map[string]string{}
+	urls := []string{
+		"https://www.google.com",
+		"https://www.amazon.com",
+		"https://www.reddit.com",
+		"https://www.facebook.com",
+		"https://www.instagram.com",
+	}
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking URL: ", url)
+
+	resp, err := http.Get(url)
+
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println("hitURL error: ", err, resp.StatusCode)
+		return errRequestFailed
+	}
+
+	return nil
+}
